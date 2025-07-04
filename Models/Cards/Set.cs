c37300740cb74c2c956de14c656039aee29e25f5
Tommy Webb <tommygeorgewebb@googlemail.com>
2025-07-04 19:49:08 +0100

    Added expansion to set

--- Models/Cards/Set.cs
+++ Models/Cards/Set.cs
@@ -4,6 +4,7 @@ public class Set
 {
     public required string Name { get; set; }
     public required string Identifier { get; set; }
+    public required Expansion Expansion { get; set; }
     
     public DateTime? ReleaseDate { get; set; }
     public int? TotalCards { get; set; }
