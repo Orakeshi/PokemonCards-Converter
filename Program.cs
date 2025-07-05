c1317756709b33b73e23cd47a54978228babba92
Tommy Webb <tommygeorgewebb@googlemail.com>
2025-07-05 04:44:00 +0100

    Cleanup program file & move logic to transformer

--- Program.cs
+++ Program.cs
@@ -13,50 +13,10 @@ class Program
     static void Main(string[] args)
     {
         CsvService<PokemonCardCsv> csvService = new CsvService<PokemonCardCsv>();
+        PokemonCardTransformer transformer = new PokemonCardTransformer();
         
-        var rawCards = csvService.Read("/Users/tommy/Documents/Dev/Personal/Coding/Projects/Pokemon/Pokemon Cards/PokemonCards-Converter/Data/Cards.csv", new PokemonCardMap());
-        List<Card> cards = new List<Card>();
-        
-        foreach (PokemonCardCsv rawCard in rawCards)
-        {
-            Card card = new Card
-            {
-                Number = int.TryParse(rawCard.Number, out var number) ? number : 0,
-                Name = rawCard.Name,
-                Type = EnumParser.CardTypeParser(rawCard.Type),
-                Storage = EnumParser.StorageTypeParser(rawCard.Storage),
-                
-                IsCollected = rawCard.Collected.ToUpper() == "TRUE",
-                AcquireType = EnumParser.AcquireTypeParser(rawCard.AcquireType),
-                CollectionDate = !string.IsNullOrWhiteSpace(rawCard.CollectionDate)
-                    ? DateTime.ParseExact(rawCard.CollectionDate, "dd/MM/yyyy", CultureInfo.InvariantCulture)
-                    : null,
-                DuplicateAmount = int.TryParse(rawCard.DuplicateAmount, out var duplicateNumber) ? duplicateNumber : 0,
-            };
-
-            cards.Add(card);
-
-            if (rawCard.RHCollected.ToUpper() == "TRUE")
-            {
-                Card rhCard = new Card
-                {
-                    Number = card.Number,
-                    Name = card.Name,
-                    Type = CardType.ReverseHolo,
-                    Storage = card.Storage,
-                
-                    IsCollected = true,
-                    AcquireType = EnumParser.AcquireTypeParser(rawCard.RHAcquireType),
-                    CollectionDate = !string.IsNullOrWhiteSpace(rawCard.RHCollectionDate)
-                        ? DateTime.ParseExact(rawCard.RHCollectionDate, "dd/MM/yyyy", CultureInfo.InvariantCulture)
-                        : null,
-                    DuplicateAmount = int.TryParse(rawCard.RHDuplicateAmount, out var rhDuplicateNumber) ? rhDuplicateNumber : 0,
-                };
-                
-                cards.Add(rhCard);
-            }
-            
-        }
+        List<PokemonCardCsv> rawCards = csvService.Read("/Users/tommy/Documents/Dev/Personal/Coding/Projects/Pokemon/Pokemon Cards/PokemonCards-Converter/Data/Cards.csv", new PokemonCardMap());
+        var cards = transformer.TransformCards(rawCards);
         
         foreach(var card in cards)
         {
---
+++ Services/PokemonCardTransformer.cs
@@ -0,0 +1,72 @@
+using System.Globalization;
+using PokemonCardConverter.Enums;
+using PokemonCardConverter.Helpers;
+using PokemonCardConverter.Models;
+using PokemonCardConverter.Models.Cards;
+
+namespace PokemonCardConverter.Services;
+
+public class PokemonCardTransformer
+{
+    public List<Card> TransformCards(List<PokemonCardCsv> rawCards)
+    {
+        List<Card> cards = new List<Card>();
+        
+        foreach (PokemonCardCsv rawCard in rawCards)
+        {
+            Card card = CreateCard(rawCard);
+            cards.Add(card);
+
+            if (rawCard.RHCollected == "-") continue;
+            
+            Card rhCard = CreateReverseHoloCard(rawCard);
+            cards.Add(rhCard);
+
+        }
+        
+        return cards;
+    }
+
+    private Card CreateCard(PokemonCardCsv rawCard)
+    {
+        Card card = SetupInitialCard(rawCard);
+
+        card.IsCollected = rawCard.Collected.ToUpper() == "TRUE";
+        card.AcquireType = EnumParser.AcquireTypeParser(rawCard.AcquireType);
+        card.CollectionDate = ParseDate(rawCard.CollectionDate);
+        card.DuplicateAmount = int.TryParse(rawCard.DuplicateAmount, out var duplicateNumber) ? duplicateNumber : 0;
+        
+        return card;
+    }
+
+    private Card CreateReverseHoloCard(PokemonCardCsv rawCard)
+    {
+        Card rhCard = SetupInitialCard(rawCard);
+        rhCard.Type = CardType.ReverseHolo;
+        
+        rhCard.IsCollected = rawCard.RHCollected.ToUpper() == "TRUE";
+        rhCard.AcquireType = EnumParser.AcquireTypeParser(rawCard.RHAcquireType);
+        rhCard.CollectionDate = ParseDate(rawCard.RHCollectionDate);
+        rhCard.DuplicateAmount = int.TryParse(rawCard.RHDuplicateAmount, out var rhDuplicateNumber) ? rhDuplicateNumber : 0;
+        
+        return rhCard;
+    }
+
+    private Card SetupInitialCard(PokemonCardCsv rawCard)
+    {
+        return new Card
+        {
+            Number = int.TryParse(rawCard.Number, out var number) ? number : 0,
+            Name = rawCard.Name,
+            Type = EnumParser.CardTypeParser(rawCard.Type),
+            Storage = EnumParser.StorageTypeParser(rawCard.Storage),
+        };
+    }
+    
+    private DateTime? ParseDate(string? input)
+    {
+        return !string.IsNullOrWhiteSpace(input)
+            ? DateTime.ParseExact(input, "dd/MM/yyyy", CultureInfo.InvariantCulture)
+            : null;
+    }
+}
